e732f5ee4f6496c3ae9b1ffa699ac5f3d0311bf7
nparamonov <nikita@nparamonov.ru>
2024-03-10 23:47:46 +0300

    tests(e2e): добавлен тест для метода /service/ping

---
+++ tests/e2e/test_service.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from starlette.status import HTTP_200_OK
+
+
+def test_ping(client: TestClient) -> None:
+    """Test /service/ping"""
+    response = client.get("/service/ping")
+    assert response.status_code == HTTP_200_OK
+    assert response.text == "pong"
