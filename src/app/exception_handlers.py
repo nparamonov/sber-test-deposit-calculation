3264d490a91882f6bbc7987c4900134e73e8276f
nparamonov <nikita@nparamonov.ru>
2024-03-11 18:43:33 +0300

    fix(app): убран лишний параметр в ответе с ошибкой валидации

--- src/app/exception_handlers.py
+++ src/app/exception_handlers.py
@@ -1,28 +1,27 @@
 from collections.abc import Callable, Coroutine
 from typing import Any
 
-from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 from starlette.status import HTTP_400_BAD_REQUEST
 
 
-async def html_request_validation_exception_handler(_request: Request, exc: RequestValidationError) -> JSONResponse:
+async def request_validation_exception_handler(_request: Request, exc: RequestValidationError) -> JSONResponse:
     """Обработчик ошибки RequestValidationError
 
     По условиям задачи должен быть ключ "error" с текстом ошибки, поэтому вытаскиваем сообщение от pydantic.
-    Но оставим подробную структуру "detail" для удобства.
+    При необходимости, подробную структуру можно получить: fastapi.encoders.jsonable_encoder(exc.errors())
     """
     msg = ". ".join([f"{error['msg']}: {error['loc']} = {error['input']}" for error in exc.errors()])
 
     return JSONResponse(
         status_code=HTTP_400_BAD_REQUEST,
-        content={"error": msg, "detail": jsonable_encoder(exc.errors())},
+        content={"error": msg},
     )
 
 exception_handlers: dict[int | type[Exception], Callable[[Request, Any], Coroutine[Any, Any, Response]]] = {
-    RequestValidationError: html_request_validation_exception_handler,
+    RequestValidationError: request_validation_exception_handler,
 }
 
 openapi_responses: dict[int | str, dict[str, Any]] = {
